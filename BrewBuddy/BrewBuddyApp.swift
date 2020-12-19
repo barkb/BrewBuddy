b7b8e8e9a820557abf15ee9db44f02f93a1baf2f
Ben Barkett <benbarkett@gmail.com>
2020-12-19 14:14:00 -0600

    Initial Commit

---
+++ BrewBuddy/BrewBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewBuddyApp.swift
+//  BrewBuddy
+//
+//  Created by Ben Barkett on 12/19/20.
+//
+
+import SwiftUI
+
+@main
+struct BrewBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
