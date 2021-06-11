55b3cecf89a3f035b8e259b338073a773b7ba0ac
Ben Barkett <benbarkett@gmail.com>
2021-06-11 11:51:39 -0500

    Added haptics to close playlist button

--- BrewBuddy/Tabs/Playlists/EditPlaylistView.swift
+++ BrewBuddy/Tabs/Playlists/EditPlaylistView.swift
@@ -6,9 +6,10 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct EditPlaylistView: View {
-    let playlist: Playlist
+    @ObservedObject var playlist: Playlist
 
     @EnvironmentObject var dataController: DataController
     @Environment(\.presentationMode) var presentationMode
@@ -17,6 +18,8 @@ struct EditPlaylistView: View {
     @State private var detail: String
     @State private var color: String
     @State private var showingDeleteConfirm = false
+
+    @State private var engine = try? CHHapticEngine()
 
     let colorColumns = [
         GridItem(.adaptive(minimum: 44))
@@ -44,10 +47,7 @@ struct EditPlaylistView: View {
             } // Section 2
             // swiftlint:disable:next line_length
             Section(footer: Text("Closing a playlist moves it from the Open to Closed tab; deleting it removes the playlist entirely.")) {
-                Button(playlist.isActive ? "Close this playlist" : "Reopen this playlist") {
-                    playlist.isActive.toggle()
-                    update()
-                } // Close Button
+                Button(playlist.isActive ? "Close this playlist" : "Reopen this playlist", action: toggleClosed)
                 Button("Delete this playlist") {
                     showingDeleteConfirm.toggle()
                 }
@@ -63,6 +63,46 @@ struct EditPlaylistView: View {
                 primaryButton: .default(Text("Delete"), action: delete),
                 secondaryButton: .cancel()
             )
+        }
+    }
+
+    func toggleClosed() {
+       playlist.isActive.toggle()
+        if !playlist.isActive {
+            do {
+                try engine?.start()
+                let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0.5)
+                let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+
+                let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
+                let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
+
+                let parameter = CHHapticParameterCurve(
+                    parameterID: .hapticIntensityControl,
+                    controlPoints: [start, end],
+                    relativeTime: 0
+                )
+
+                let event1 = CHHapticEvent(
+                    eventType: .hapticTransient,
+                    parameters: [intensity, sharpness],
+                    relativeTime: 0
+                )
+
+                let event2 = CHHapticEvent(
+                    eventType: .hapticContinuous,
+                    parameters: [sharpness, intensity],
+                    relativeTime: 0.125,
+                    duration: 1
+                )
+
+                let pattern = try CHHapticPattern(events: [event1, event2], parameterCurves: [parameter])
+
+                let player = try engine?.makePlayer(with: pattern)
+                try player?.start(atTime: 0)
+            } catch {
+                // playing haptics didnt work
+            }
         }
     }
 
